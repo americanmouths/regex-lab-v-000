a0c484b7b2841c3b8491466ebbe20889d44031a3
Kayla Hansen <shiverr@gmail.com>
2017-10-15 04:46:14 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,8 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\b[aeiou]/)
+  if word.match(/\b[aAeEiIoOuU]/)
     true
   else
     false
-  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
